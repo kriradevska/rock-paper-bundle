f2b6b73246ec2e8bedfa7e5596aa756230f48a85
Kristina Radevska <kriradevska@gmail.com>
2022-11-23 21:52:10 +0100

    html-webpack-plugin install, error 404 fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
     mode: 'development',
     entry: './code/main.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+      },
+      plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
     module: {
         rules: [
             {
